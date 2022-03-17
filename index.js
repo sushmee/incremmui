a6326e4ae8e5a5c3bdb44f9abc90644974199a65
sushmee <86013254+sushmee@users.noreply.github.com>
2022-03-17 16:35:36 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Challenge from "./Challenge";
+
+ReactDOM.render(
+<Challenge/>,
+    document.getElementById("root")
+);
